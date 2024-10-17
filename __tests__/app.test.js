4445af0896165df4f9cd9b8ad7ea40eaa3da3b2a
lavanyasoundar17 <lavanyaprdp17@gmail.com>
2024-10-17 14:56:35 +0100

    added user test case in app.test file

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -304,4 +304,22 @@ describe('DELETE /api/comments/:comment_id', () => {
     });
 });
 
+//task 10
+describe('/api/users',()=>{
+    test('200 : get all users',()=>{
+        return request(app)
+        .get('/api/users')
+        .expect(200)
+        .then(({body})=>{
+            body.users.forEach((user)=>{
+                expect(user).toMatchObject({
+                    username:expect.any(String),
+                    name:expect.any(String),
+                    avatar_url:expect.any(String)
+                })
+            })
+        })
+    })
+})
+
 
